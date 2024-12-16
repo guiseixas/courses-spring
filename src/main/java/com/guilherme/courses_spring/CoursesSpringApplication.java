120eff6064795e875d35b4854a76e91a26e9dda4
guilherme seixas <guilhermesantoseixas@outlook.com>
2024-12-16 20:38:23 -0300

    first commit

---
+++ src/main/java/com/guilherme/courses_spring/CoursesSpringApplication.java
@@ -0,0 +1,13 @@
+package com.guilherme.courses_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursesSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoursesSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/guilherme/courses_spring/CoursesSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guilherme.courses_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursesSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
